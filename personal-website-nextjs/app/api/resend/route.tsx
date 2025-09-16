570c0c4a30ea27266c6dc6426c338b6ec5b87deb
AdrianTodd <athomesoftwaredevelopment@gmail.com>
2025-09-16 10:15:25 -0600

    test

--- personal-website-nextjs/app/api/resend/route.tsx
+++ personal-website-nextjs/app/api/resend/route.tsx
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY as string);
 const fromEmail = process.env.FROM_EMAIL as string;
-console.log(fromEmail);
+console.log(resend);
 export async function POST(req: NextRequest) {
   if (!process.env.RESEND_API_KEY) {
     return new NextResponse("Missing RESEND_API_KEY", { status: 500 });
@@ -18,7 +18,7 @@ export async function POST(req: NextRequest) {
     console.log(email, subject, message);
     const data = await resend.emails.send({
       from: fromEmail,
-      to: ["delivered@resend.dev"],
+      to: ["todd.r.adrian@gmail.com"],
       subject: subject,
       react: (
         <>
