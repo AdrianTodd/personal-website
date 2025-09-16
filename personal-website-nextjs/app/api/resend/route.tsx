34233b12492351d8c6c8c8b3dc179226c592f6e3
AdrianTodd <athomesoftwaredevelopment@gmail.com>
2025-09-15 22:45:37 -0600

    test fix

--- personal-website-nextjs/app/api/resend/route.tsx
+++ personal-website-nextjs/app/api/resend/route.tsx
@@ -18,7 +18,7 @@ export async function POST(req: NextRequest) {
     console.log(email, subject, message);
     const data = await resend.emails.send({
       from: fromEmail,
-      to: "todd.r.adrian@gmail.com",
+      to: ["todd.r.adrian@gmail.com"],
       subject: subject,
       react: (
         <>
